7c2a56110428cb14722fe00f4a73701983cd4472
the-uncanny-valley <runcannyvalley@gmail.com>
2025-07-13 17:02:37 +0300

    Update MainActivity to display notes onResume and open AddNoteActivity
    Set up button listener to open AddNoteActivity.
    Refresh note list in onResume to reflect newly added notes.
    Inflate note_item layout and apply background color based on note's color.

--- app/src/main/java/com/example/unicornshoppinglist/MainActivity.java
+++ app/src/main/java/com/example/unicornshoppinglist/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.unicornshoppinglist;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class MainActivity extends AppCompatActivity {
+
+    private LinearLayout linearLayoutNotes;
+    private FloatingActionButton buttonAddNote;
+
+    private Database database = Database.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +32,50 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        initViews();
+
+        buttonAddNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = AddNoteActivity.newIntent(MainActivity.this);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        showNotes();
+    }
+
+    private void initViews() {
+        linearLayoutNotes = findViewById(R.id.linearLayoutNotes);
+        buttonAddNote = findViewById(R.id.buttonAddNote);
+    }
+
+    private void showNotes() {
+        linearLayoutNotes.removeAllViews();
+        for (Note note : database.getNotes()) {
+            View view = getLayoutInflater().inflate(R.layout.note_item, linearLayoutNotes, false);
+
+            TextView textViewNote = view.findViewById(R.id.textViewNote);
+            textViewNote.setText(note.getText());
+
+            int colorResId;
+
+            if (note.getColor().equals("pink")) {
+                colorResId = R.color.surface_default;
+            } else if (note.getColor().equals("blue")) {
+                colorResId = R.color.surface_uranian_blue;
+            } else {
+                colorResId = R.color.surface_tropical_indigo;
+            }
+
+            int color = ContextCompat.getColor(this, colorResId);
+            textViewNote.setBackgroundColor(color);
+
+            linearLayoutNotes.addView(view);
+        }
     }
 }
