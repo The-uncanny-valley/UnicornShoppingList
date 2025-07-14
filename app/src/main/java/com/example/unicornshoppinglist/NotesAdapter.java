b745ccea116682aed0bbe9eb4a0bebc5f6c4e95d
the-uncanny-valley <runcannyvalley@gmail.com>
2025-07-14 20:40:39 +0300

    feat: add NotesAdapter for displaying colored note items in RecyclerView

---
+++ app/src/main/java/com/example/unicornshoppinglist/NotesAdapter.java
@@ -0,0 +1,65 @@
+package com.example.unicornshoppinglist;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.NotesViewHolder> {
+    private ArrayList<Note> notes = new ArrayList<>();
+
+    private void setNotes(ArrayList<Note> notes) {
+        this.notes = notes;
+    }
+
+    @NonNull
+
+    @Override
+    public NotesViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(
+                R.layout.note_item,
+                parent,
+                false
+        );
+        return new NotesViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NotesViewHolder viewHolder, int position) {
+        Note note = notes.get(position);
+        viewHolder.textViewNote.setText(note.getText());
+
+        int colorResId;
+
+        if (note.getColor().equals("pink")) {
+            colorResId = R.color.surface_default;
+        } else if (note.getColor().equals("blue")) {
+            colorResId = R.color.surface_uranian_blue;
+        } else {
+            colorResId = R.color.surface_tropical_indigo;
+        }
+
+        int color = ContextCompat.getColor(viewHolder.itemView.getContext(), colorResId);
+        viewHolder.textViewNote.setBackgroundColor(color);
+    }
+
+    @Override
+    public int getItemCount() {
+        return notes.size();
+    }
+
+    class NotesViewHolder extends RecyclerView.ViewHolder {
+        private TextView textViewNote;
+
+        public NotesViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textViewNote = itemView.findViewById(R.id.textViewNote);
+        }
+    }
+}
