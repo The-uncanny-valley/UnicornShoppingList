f856563fc5a023952be5dd19ee8385be9db7c57c
the-uncanny-valley <runcannyvalley@gmail.com>
2025-08-09 15:33:12 +0300

    Add Room entity Note and NoteDatabase singleton implementation

--- app/src/main/java/com/example/unicornshoppinglist/Note.java
+++ app/src/main/java/com/example/unicornshoppinglist/Note.java
@@ -1,9 +1,15 @@
 package com.example.unicornshoppinglist;
 
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "notes")
 public class Note {
-    private int id;
-    private String text;
-    private String color;
+
+    @PrimaryKey(autoGenerate = true)
+    private final int id;
+    private final String text;
+    private final String color;
 
     public Note(int id, String text, String color) {
         this.id = id;
